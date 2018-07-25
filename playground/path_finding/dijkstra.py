97963d03553a49ced23f64c3b0182cf3b5690098
Kravchenko Fyodor <udalite.feda@hotmail.com>
2018-07-25 23:52:31 +0800

    add dijkastra for adjacency matrix

--- playground/path_finding/dijkstra.py
+++ playground/path_finding/dijkstra.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
 # Dijkastra alg based on heap queue
-# With adjacency-list representation
+# With adjacency-list representation,
+# and adjacency matrix one
+# mostly based on https://www.geeksforgeeks.org/greedy-algorithms-set-6-dijkstras-shortest-path-algorithm
 from collections import defaultdict
 from heapq import heappop, heappush
+
+class Graph():
+  # class for adjacency path representation
+  # and min distance solving
+
+  def __init__(self, verticies):
+    self.V = verticies
+    self.graph = [[0 for col in range(verticies)] for row in range(verticies)]
+
+  def minDistance(self, dist, setV):
+    min_dist = float("inf")
+    for verticie in range(self.V):
+      if dist[verticie] < min_dist and setV[verticie] == False:
+        min_dist = dist[verticie]
+        min_index = verticie
+
+    return min_index
+
+  def solve(self, start):
+    dist = [float("inf")] * self.V
+    dist[start] = 0
+    setV = [False] * self.V
+    for cout in range(self.V):
+      u = self.minDistance(dist, setV)
+      setV[u] = True
+      for v in range(self.V):
+        if self.graph[u][v] > 0 and setV[v] == False and dist[v] > dist[u] + self.graph[u][v]:
+          dist[v] = dist[u] + self.graph[u][v]
+
+    for node in range(self.V):
+      print(node, "t", dist[node])
+
+    return
 
 def dijkstra(edges, f, t):
   g = defaultdict(list)
@@ -46,3 +81,16 @@ if __name__ == "__main__":
   print(dijkstra(edges, "A", "E"))
   print("A -> G:")
   print(dijkstra(edges, "A", "G"))
+  
+  g  = Graph(9)
+  g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+            [4, 0, 8, 0, 0, 0, 0, 11, 0],
+            [0, 8, 0, 7, 0, 4, 0, 0, 2],
+            [0, 0, 7, 0, 9, 14, 0, 0, 0],
+            [0, 0, 0, 9, 0, 10, 0, 0, 0],
+            [0, 0, 4, 14, 10, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 2, 0, 1, 6],
+            [8, 11, 0, 0, 0, 0, 1, 0, 7],
+            [0, 0, 2, 0, 0, 0, 6, 7, 0]];
+
+  g.solve(0)
