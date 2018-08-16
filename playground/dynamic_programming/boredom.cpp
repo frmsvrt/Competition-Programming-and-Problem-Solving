b93e41e6b8d0c9575d9420816c5936c82060f16d
Fydor Kravchenko <udalite.feda@hotmail.com>
2018-08-16 23:48:51 +0800

    add template and part of boredom prob solution

---
+++ playground/dynamic_programming/boredom.cpp
@@ -0,0 +1,40 @@
+// boredom.cpp
+// Given a seq `a` of `n` integers
+// Choose one element of `a` to delete, 
+// then all `a_k+1` and `a_k-1` will be deleted too
+// that will brind a_k points
+// maximize points
+
+#include <iostream>
+#include <algorithm>
+#include <limits.h>
+#include <stdio.h>
+#include <map>
+
+int solve(int arr[], int size) {
+	return 0;
+}
+
+int main() {
+	int n, ret;
+	std::cin >> n;
+	std::map<int, int> freq;
+
+	// paste elements into seq
+	// count it freq
+	int seq[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> seq[i];
+		freq[seq[i]]++;
+	}
+
+	// max el of array
+	auto max = *std::max_element(seq, seq+n);
+
+	// go through the freq
+	for(std::map<int,int>:: iterator it = freq.begin(); it != freq.end(); it++) {
+    	std::cout<<it->first<<" "<<it->second<<std::endl; 
+	}
+
+	return 0;
+} 
---
+++ template.cpp
@@ -0,0 +1,31 @@
+// template.cpp
+// template file for problim solving
+// consist of main function that read size of array
+// and then fill this array from cmd
+// and solve method, empty ofcourse
+
+#include <iostream>
+#include <algorithm>
+#include <limits.h>
+#include <stdio.h>
+
+int solve(int arr[], int size) {
+	return 0;
+}
+
+int main() {
+	int n, ret;
+	std::cin >> n;
+
+	// paste elements into seq
+	int seq[n];
+	for (int i = 0; i < n; i++) {
+		std::cin >> seq[i];
+	}
+
+	// just for debug
+	for (auto& x : seq) std::cout << x;
+	std::cout << "\n";
+
+	return 0;
+} 
